b460b14a6cf648ebce04ef882d407ea5c77b469c
westvillege <73535181+westvillege@users.noreply.github.com>
2020-12-31 18:48:34 +0900

    バリデーションの設定の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable,  :validatable
 
-　validates :name, presence: true
+  validates :name, presence: true
 
 end
