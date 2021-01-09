7221e4566cfeacfdf74dc29de9f4ab8ee96691a5
westvillege <73535181+westvillege@users.noreply.github.com>
2021-01-10 07:47:56 +0900

    チャットルームの削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,6 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
-      @messages = @room.messages.includes(:user)
       render :index
     end
   end
